b2756f1594681082d6217894a7e264373fb65044
alexandreoliveirasapiranga <33130258+alexandreoliveirasapiranga@users.noreply.github.com>
2021-01-06 07:22:12 -0300

    Classe de Conexão
    Classe de conexão com o banco de dados SQL.

---
+++ conexao.cs
@@ -0,0 +1,14 @@
+//Classe de conexão com o banco de dados
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace camadaDados
+{
+    class conexao
+    {        
+        public static string Cn = @"Data Source = DESKTOP-UE2G19Q; Integrated Security = True";
+    }
+}
